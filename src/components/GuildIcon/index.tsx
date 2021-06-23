ded9418fdb4c8ed426e6f1b0584da9d06d29ac9e
pcampina <pablo@campina.dev>
2021-06-23 19:01:39 -0300

    Class 02 - Maximum Speed

---
+++ src/components/GuildIcon/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { styles } from './styles';
+
+export function GuildIcon() {
+  const uri = 'https://yt3.ggpht.com/ytc/AKedOLQc1OCf9gztVmcVnmI_41uN9axrRP8wd4a-GflFRQ=s900-c-k-c0x00ffffff-no-rj';
+
+  return (
+    <Image
+      source={{ uri }}
+      style={styles.image}
+      resizeMode="cover"
+    />
+  )
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {
   View,
   Text,
-  Image,
-  StatusBar
+  Image
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 
@@ -12,14 +12,14 @@ import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle={'light-content'}
-        backgroundColor="transparent"
-        translucent
-      />
-      
       <Image
         source={IllustrationImg}
         style={styles.image}
@@ -28,19 +28,19 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize{`\n`}
-          suas jogatinas{`\n`}
-          facilmente
+          Conecte-se{'\n'}
+          e organize suas{'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games{`\n`}
+          Crie grupos para jogar seus games{'\n'}
           favoritos com seus amigos
         </Text>
 
         <ButtonIcon
           title="Entrar com o Discord"
-          activeOpacity={0.7}
+          onPress={handleSignIn}
         />
       </View>
     </View>
